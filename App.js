76fa23955e2a885defef16cc304a92435c0f5dff
ac-opensource <a-ar@outlook.com>
2017-03-24 15:34:44 +0800

    Ejected from CRNA

--- App.js
+++ App.js
@@ -1,17 +1,94 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, ListItem, List, Switch, Thumbnail } from 'native-base';
 
 export default class App extends React.Component {
-  render() {
+    render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <Container>
+            <Header>
+                <Left>
+                    <Button transparent>
+                        <Icon name='menu' />
+                    </Button>
+                </Left>
+                <Body>
+                    <Title>Usapang Tambay</Title>
+                </Body>
+                <Right />
+            </Header>
+
+            <Content>
+
+                <List dataArray={items} renderRow={(data) =>
+                        <ListItem avatar>
+                    <Left>
+                        <Thumbnail source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}} />
+                    </Left>
+                    <Body>
+                    <Text>{data.name}</Text>
+                    <Text note>{data.title}</Text>
+                    </Body>
+                    <Right>
+                        <Text note>3:43 pm</Text>
+                    </Right>
+                </ListItem>
+                    } />
+
+            </Content>
+
+            <Footer>
+                <FooterTab>
+                    <Button full>
+                        <Text style={{color: '#fff'}}>New Post</Text>
+                    </Button>
+                </FooterTab>
+            </Footer>
+        </Container>
     );
   }
 }
+const items = [{name: `John Doe`, title: `Doing what you like will always keep you happy.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
+    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`}
+];
 
 const styles = StyleSheet.create({
   container: {
