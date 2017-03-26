114386d8053967e3a849f6ba119efd8e9595290a
ac-opensource <a-ar@outlook.com>
2017-03-26 19:55:04 +0800

    Add a simple fetch request (to be replaced by our API when ready)

--- App.js
+++ App.js
@@ -1,100 +1,109 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, ListItem, List, Switch, Thumbnail } from 'native-base';
+import {Text} from 'react-native';
+import {
+    Container,
+    Header,
+    Title,
+    Content,
+    Footer,
+    FooterTab,
+    Button,
+    Left,
+    Right,
+    Body,
+    Icon,
+    ListItem,
+    List,
+    Spinner,
+    Thumbnail
+} from 'native-base';
 
 export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: 'platf0rm',
+            results: {
+                items: []
+            }
+        }
+    }
+
+    search() {
+        // Set loading to true when the search starts to display a Spinner
+        this.setState({
+            loading: true
+        });
+
+        const that = this;
+        // TODO: Replace with our API
+        return fetch('https://api.github.com/search/repositories?q=' + this.state.search)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                // Store the results in the state variable results and set loading to
+                // false to remove the spinner and display the list of repositories
+                that.setState({
+                    results: responseJson,
+                    loading: false
+                });
+
+                return responseJson.Search;
+            })
+            .catch((error) => {
+                that.setState({
+                    loading: false
+                });
+
+                console.error(error);
+            });
+    }
+
+    componentDidMount() {
+        this.search();
+    }
+
     render() {
-    return (
-        <Container>
-            <Header>
-                <Left>
-                    <Button transparent>
-                        <Icon name='menu' />
-                    </Button>
-                </Left>
-                <Body>
-                    <Title>Usapang Tambay</Title>
-                </Body>
-                <Right />
-            </Header>
-
-            <Content>
-
-                <List dataArray={items} renderRow={(data) =>
-                        <ListItem avatar>
+        return (
+            <Container>
+                <Header>
                     <Left>
-                        <Thumbnail source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}} />
+                        <Button transparent>
+                            <Icon name='menu'/>
+                        </Button>
                     </Left>
                     <Body>
-                    <Text>{data.name}</Text>
-                    <Text note>{data.title}</Text>
+                    <Title>Usapang Tambay</Title>
                     </Body>
-                    <Right>
-                        <Text note>3:43 pm</Text>
-                    </Right>
-                </ListItem>
-                    } />
+                    <Right />
+                </Header>
 
-            </Content>
+                <Content>
+                    {this.state.loading? <Spinner /> : <List dataArray={this.state.results.items} renderRow={(data) =>
+                        <ListItem avatar>
+                            <Left>
+                                <Thumbnail source={{uri: data.owner.avatar_url}} />
+                            </Left>
+                            <Body>
+                                <Text>{data.login}</Text>
+                                <Text note>{data.full_name}</Text>
+                            </Body>
+                            <Right>
+                                <Text note>3:43 pm</Text>
+                            </Right>
+                        </ListItem>
+                    }/>}
 
-            <Footer>
-                <FooterTab>
-                    <Button full>
-                        <Text style={{color: '#fff'}}>New Post</Text>
-                    </Button>
-                </FooterTab>
-            </Footer>
-        </Container>
-    );
-  }
+                </Content>
+
+                <Footer>
+                    <FooterTab>
+                        <Button full>
+                            <Text style={{color: '#fff'}}>New Post</Text>
+                        </Button>
+                    </FooterTab>
+                </Footer>
+            </Container>
+        );
+    }
 }
-const items = [{name: `John Doe`, title: `Doing what you like will always keep you happy.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`},
-    {name: `John Smith`, title: `Don't let small minds convince you that your dreams are too big.`}
-];
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
